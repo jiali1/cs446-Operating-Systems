25be0c2494d8f3b75a6f28796e67779c9dc58ffb
Jia Li <jiali@nevada.unr.edu>
2017-08-29 14:14:44 -0700

    first commit

---
+++ cs446/Sim04_LiJia/MemoryFunction.h
@@ -0,0 +1,34 @@
+// Program Information /////////////////////////////////////////////////////////
+/**
+ * @file Simulator Functions.cpp
+ *
+ * @brief Memory Address Locator for CS 446/646 at the Univserity of 
+ *        Nevada, Reno
+ * 
+ * @details This file includes the necessary function for assignment 2 of the 
+ *          CS 446/646 course simulator.
+ *
+ * @author Vineeth Rajamohan
+ *
+ * @version 1.01 (27 Jan 17)
+ */
+
+// PRECOMPILER DIRECTIVES //////////////////////////////////////////////////////
+
+#ifndef MEM_FUNC_H
+#define MEM_FUNC_H
+
+// HEADER FILES ////////////////////////////////////////////////////////////////
+
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <sys/time.h>
+
+// GLOBAL CONSTANTS ////////////////////////////////////////////////////////////
+
+ // None
+
+unsigned int allocateMemory( int prevAddress, int blockSize, int memorySize );
+
+#endif // MEM_FUNC_H
